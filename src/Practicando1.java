01d63f08638721981ec611393b244b849672aaf5
Neidys22 <neidysandoval22@hotmail.com>
2021-07-08 13:23:50 -0500

    Elimine una linea de mi archivo

--- src/Practicando1.java
+++ src/Practicando1.java
@@ -2,7 +2,6 @@ public class Practicando1 {
     
     public static void main(String[] args) throws Exception {
         if (5>3 ^ 5>9){  //solo una puede ser verdad no las dos (^ indica o)- o especial
-            System.out.println(":)");
             System.out.println("mi primer commit");
     
     }
